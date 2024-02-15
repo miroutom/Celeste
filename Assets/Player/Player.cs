8da62b8f51f4078d6a5b73217c2aed919a90bf0d
Kirill <98475298+Metacometa@users.noreply.github.com>
2024-02-15 17:18:29 +0300

    Add smoke after jump (#11)
    * Fix camera-jumping bug
    
    * Add jump smoke
    
    * Add experimental parallax effect

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -38,11 +38,18 @@ public class Player : MonoBehaviour
 
     [Header("Debug")]
     [SerializeField] private Text stateText;
+    [SerializeField] private Text onGroundText;
     private string textState = "";
+    private string textOnGround = "";
 
     private bool onWall;
     private bool onGround;
     private bool onPullUp;
+
+    private bool jumpPressed;
+    private bool grabPressed;
+    private bool climbUp;
+    private bool climbDown;
 
     private bool pullUp = false;
 
@@ -62,6 +69,18 @@ public class Player : MonoBehaviour
     [SerializeField] private Vector2 rightMiddleOffset; 
     [SerializeField] private Vector2 leftBottomOffset; 
     [SerializeField] private Vector2 leftMiddleOffset; 
+
+    [Header("Particles")]
+    [SerializeField] private GameObject jumpSmoke;
+    [SerializeField] private Vector3 jumpSmokeOffset; 
+
+    [Header("Pseudo Parallax")]
+    [SerializeField] private GameObject clouds;
+    [SerializeField] private GameObject mountains; 
+    [SerializeField] private GameObject grass;
+    [SerializeField] private float cloudsMoveSpeed;
+    [SerializeField] private float mountainsMoveSpeed;
+    [SerializeField] private float grassMoveSpeed;
 
     void Start()
     {
@@ -88,9 +107,28 @@ public class Player : MonoBehaviour
                     (!Physics2D.OverlapCircle((Vector2)transform.position + leftMiddleOffset, collisionRadius, groundLayer) &&
                     Physics2D.OverlapCircle((Vector2)transform.position + leftBottomOffset, collisionRadius, groundLayer));
 
+        jumpPressed = Input.GetKeyDown(KeyCode.Space);
+        grabPressed = Input.GetKey(KeyCode.LeftControl);
+        climbUp = Input.GetKey(KeyCode.UpArrow);
+        climbDown = Input.GetKey(KeyCode.DownArrow);
+
         //Debug.Log("On ground: " + onGround);
         //Debug.Log("On wall: " + onWall);
         Debug.Log("On pull up: " + onPullUp);
+
+
+        //Debug
+        stateText.text = "State: " + textState;
+
+        if (jumpPressed)
+        {
+            textOnGround = "True";
+        }
+        else
+        {
+            textOnGround = "False";
+        }
+        onGroundText.text = "OnGround: " + textOnGround;
     }
 
     void FixedUpdate()
@@ -99,8 +137,7 @@ public class Player : MonoBehaviour
         Move();
         Flip();
 
-        //Debug
-        stateText.text = "State: " + textState;
+        PseudoParallax();
     }
 
     private State getState()
@@ -111,10 +148,7 @@ public class Player : MonoBehaviour
             return State.death;
         }
 
-        bool jumpPressed = Input.GetKeyDown(KeyCode.Space);
-        bool grabPressed = Input.GetKey(KeyCode.LeftControl);
-        bool climbUp = Input.GetKey(KeyCode.UpArrow);
-        bool climbDown = Input.GetKey(KeyCode.DownArrow);
+        Debug.Log(jumpPressed);
 
         rb.gravityScale = basicGravityScale;   
 
@@ -127,7 +161,7 @@ public class Player : MonoBehaviour
             //tossAsideTimer = tossAsideDelay;
         }
 
-        if (onWall && jumpPressed && state != State.jump)
+        if (onWall && jumpPressed && (state == State.grab || state == State.climbDown || state == State.climbUp))
         {
             rb.velocity = new Vector2(rb.velocity.x, 0f);
             Jump();
@@ -161,6 +195,14 @@ public class Player : MonoBehaviour
             return State.grab;   
         }
 
+        if (onGround && jumpPressed)
+        {
+            Jump();   
+            spawnJumpSmoke();
+            textState = "Jump";
+            return State.jump;
+        }
+
         if (rb.velocity.y < -jumpBorder)
         {
             textState = "Fall";
@@ -175,14 +217,6 @@ public class Player : MonoBehaviour
             return State.jump;
         }
 
-        if (onGround && jumpPressed)
-        {
-            Jump();   
-
-            textState = "Jump";
-            return State.jump;
-        }
-
         if (rb.velocity.x != 0)
         {
             textState = "Walk";
@@ -191,6 +225,13 @@ public class Player : MonoBehaviour
 
         textState = "Idle";
         return State.idle;
+    }
+
+    private void PseudoParallax()
+    {
+        clouds.transform.position += new Vector3(cloudsMoveSpeed * rb.velocity.x, 0, 0);
+        mountains.transform.position += new Vector3(mountainsMoveSpeed * rb.velocity.x, 0, 0);
+        grass.transform.position += new Vector3(grassMoveSpeed * rb.velocity.x, 0, 0);
     }
 
     private void PullUp()
@@ -291,5 +332,12 @@ public class Player : MonoBehaviour
         {
             state = State.death;
         }
+    }
+
+    //Particles
+
+    void spawnJumpSmoke()
+    {
+        Instantiate(jumpSmoke, transform.position + jumpSmokeOffset,  Quaternion.identity);
     }
 }
