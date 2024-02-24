e928a853c037823890e600e3055c7864f26d94a7
BimboToilet <106674149+BimboToilet@users.noreply.github.com>
2024-02-24 13:31:41 +0300

    Dialoguesystem (#14)
    * add dialogue system base
    
    * ready dialogue system
    
    * dialogue system fix, checkpoints added, one spike added

---
+++ Assets/DialogueSystem/Scripts/DialogueAnimator.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueAnimator : MonoBehaviour
+{
+    public DialogueTrigger dialogue;
+    public Animator dialogueStartAnimator;
+    public DialogueManager manager;
+    public bool playerIsClose;
+    public bool dialogueStarted;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        dialogueStartAnimator.SetBool("startOpen",true);
+        playerIsClose = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        dialogueStartAnimator.SetBool("startOpen", false);
+        manager.EndDialogue();
+        playerIsClose = false;
+        dialogueStarted = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && dialogueStarted)
+        {
+            manager.DisplayNextSentence();
+        }
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose && !dialogueStarted)
+        {
+            dialogue.TriggerDialogue();
+            dialogueStarted = true;
+        }
+    }
+
+}
