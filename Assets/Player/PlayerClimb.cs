7e0088c7d150863351a33c1639513dd83082acf0
Kirill <98475298+Metacometa@users.noreply.github.com>
2024-03-09 20:29:29 +0300

    Dash and player refactoring (#16)
    * Edit box collider of player
    
    * Refactoring of movement system
    
    * Refactor of moving system and edit jumping
    
    * Add additional layer on tailmap
    
    * Fix dead moving bug
    
    * Fix camera-jumping bug
    
    * Add jump smoke
    
    * Add experimental parallax effect
    
    * Add jump buffering
    
    * Add coyote time
    
    * Add landing dust
    
    * Change sorting layer of dust
    
    * Add fatigue system
    
    * save refactor
    
    * Save refactor process
    
    * Refactor player scripts
    
    * Animator refactor
    
    * Add first iteration of Dash
    
    * Add ripple effect
    
    * Fix ripple effect
    
    * Fix ripple effect
    
    * Add dash line

---
+++ Assets/Player/PlayerClimb.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClimb : MonoBehaviour
+{
+    [Header("Climb")]
+    [SerializeField] private float climbUpSpeed = 2;
+    [SerializeField] private float climbDownSpeed = -5;
+    [SerializeField] private float climbSlip = -2f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Slip()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, climbSlip);    
+    }
+
+    public void ClimbUp()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, climbUpSpeed);
+    }
+
+    public void ClimbDown()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, climbDownSpeed);
+    }
+}
---
+++ Assets/Player/PlayerJump.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    [Header("Jump")]
+    private float jumpBufferTime = 0.2f;
+    public float jumpBufferCounter;
+
+    private float coyoteTime = 0.2f;
+    public float coyoteTimeCounter;
+    
+    [SerializeField] private float jumpForce = 7;
+    [SerializeField] private float pullUpJumpForce = 5;
+    [SerializeField] public float jumpBorder = .3f;
+    [SerializeField] private float fallForce = 3;
+
+    public float basicGravityScale;
+
+    private Rigidbody2D rb;
+    private Indicators indicators;
+    private PlayerInput input;
+
+    private SpriteRenderer sprite;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        indicators = GetComponent<Indicators>();
+        input = GetComponent<PlayerInput>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        basicGravityScale = rb.gravityScale;  
+    }
+
+    public void timeCoyotize()
+    {
+        if (indicators.onGround)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+    }
+
+    public void jumpBufferize()
+    {
+        if (input.jumpPressed)
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+    }
+
+    public void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        jumpBufferCounter = 0f;
+
+        sprite.size = new Vector2(1, 0.5f);
+    }
+
+    public void pullUpJump()
+    {
+        //rb.velocity = new Vector2(rb.velocity.x, jumpForce / 1.4f);
+        rb.velocity = new Vector2(rb.velocity.x, pullUpJumpForce);
+    }
+
+    public void nullifyGravity()
+    {
+        rb.gravityScale = 0;
+    }
+
+    public void refreshGravity()
+    {
+        rb.gravityScale = basicGravityScale;
+    }
+}
